ec2943ef358d886a51beef97e0c7659a7d1481fc
Bruno Fantineli <bruno.fantinelli0@gmail.com>
2019-08-07 11:01:53 -0300

    Add files via upload

--- src/tcc/Analise.java
+++ src/tcc/Analise.java
@@ -16,26 +16,32 @@ import java.util.ArrayList;
  * @author bruno
  */
 public class Analise {
+
     //Array com as partes envolvidas
     public static ArrayList<Character> PartesEnvolvidas = new ArrayList<>();
-    
-    public void DetectarPartes(String path) throws FileNotFoundException, IOException{
+
+    public void DetectarPartes(String path) throws FileNotFoundException, IOException {
         //Carregar contrato
         BufferedReader br = new BufferedReader(new FileReader(path));
         //Ler contrato linha por linha
-        while(br.ready()){
+        while (br.ready()) {
             String linha = br.readLine();
             //Retirar espaços da linha
             linha = linha.replace(" ", "");
-            //Detectar se a parte já não foi adicionado na lista, e que o caracter é uma parte envolvida no contrato
-            if(!PartesEnvolvidas.contains(linha.charAt(1)) && Character.isLetter(linha.charAt(1))){
-                PartesEnvolvidas.add(linha.charAt(1));    
-            }
-            if(!PartesEnvolvidas.contains(linha.charAt(3)) && Character.isLetter(linha.charAt(3))){
-                PartesEnvolvidas.add(linha.charAt(3));
+            for (int i = 0; i < linha.length(); i++) {
+                //Detecta a abertura para as partes do contrato
+                if (linha.charAt(i) == '{') {
+                    //Detectar se a parte já não foi adicionado na lista
+                    if (!PartesEnvolvidas.contains(linha.charAt(i+1))) {
+                        PartesEnvolvidas.add(linha.charAt(i+1));
+                    }
+                    if (!PartesEnvolvidas.contains(linha.charAt(i+3))) {
+                        PartesEnvolvidas.add(linha.charAt(i+3));
+                    }
+                }
             }
         }
         br.close();
-    
+
     }
 }
