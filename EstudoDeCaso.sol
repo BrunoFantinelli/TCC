fdb994145d28f156516e9c8b0a840593b894a54d
Bruno Fantineli <bruno.fantinelli0@gmail.com>
2019-08-23 17:55:45 -0300

    Add files via upload

--- EstudoDeCaso.sol
+++ EstudoDeCaso.sol
@@ -1,23 +1,3 @@
 pragma solidity ^0.4.11;
 contract EstudoDeCaso {
-	address b;
-	address s;
-	address k;
-	address c;
-modifier onlyB(){
-	require(msg.sender == b);
-	_;
-}
-modifier onlyS(){
-	require(msg.sender == s);
-	_;
-}
-modifier onlyK(){
-	require(msg.sender == k);
-	_;
-}
-modifier onlyC(){
-	require(msg.sender == c);
-	_;
-}
 }
